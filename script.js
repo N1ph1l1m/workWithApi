63561e480ba6c529aac453b0b45b9e9422acddc4
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-11 22:28:46 +0300

    Create methods

--- script.js
+++ script.js
@@ -11,9 +11,16 @@ class GotService{
     getAllCharacters(){
         return this.getResource('https://www.anapioficeandfire.com/api/characters?page=5&pageSize=10');
     }
-
+    getCharecter(id){
+        return this.getResource(`https://www.anapioficeandfire.com/api/characters/${id}`);
+    }
 }
 
 const got = new GotService();
-got.getAllCharacters()
-    .then(res => console.log(res))
+ got.getAllCharacters()
+     .then(res => {
+        res.forEach(item => console.log(item.name));
+     });
+
+got.getCharecter(120)
+.then(res => console.log(res))
