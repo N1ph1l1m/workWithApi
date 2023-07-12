681c4e6b86e1faa67e73b1f57e7ecf2b2e8772fc
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-13 00:12:57 +0300

    Add img in component errorMessage, update errorMessage .css + .js

--- src/components/errorMessage/errorMessage.js
+++ src/components/errorMessage/errorMessage.js
@@ -1,6 +1,15 @@
 import React from "react";
+import './errorMessage.css'
+import img from './error.png'
 
 const ErrorMessage = () => {
-  return <span>Something goes wrong</span>;
+  return (
+    <>
+    <img src = {img} alt='Error img'></img>
+    <span>
+        Something goes wrong
+    </span>
+    </>
+  )
 };
 export default ErrorMessage;
