b3de8c6780c17ef3f9e1d9d6c26acd639bdd34fd
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-12 23:19:39 +0300

    Add spinner in project

---
+++ src/components/spinner/spiner.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './spinner.css';
+
+const Spinner = () =>{
+    return (
+        <div class="loadingio-spinner-spinner-pinpwyspvms"><div class="ldio-ho4n0npyl9n">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+    )
+}
+export default Spinner;
+
