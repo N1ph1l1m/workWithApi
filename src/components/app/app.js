ad6d6846e565d5dfc1d710e980f5da5917cc4409
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-08-08 20:46:45 +0300

    Add React-Router-Dom

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,60 +7,70 @@ import BooksPage from "../BooksPage";
 import CharactedPage from "../CharactedPage";
 import HousePage from "../HousePage/housePage";
 import gotService from "../../services/gotService";
-//import {BraouserRouter as Router, Route} from 'react-router-dom';
-
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import "../../App.css";
 
 export default class App extends Component {
   gotService = new gotService();
 
   state = {
     showRandomChar: true,
-    error:false
+    error: false,
   };
 
-  componentDidCatch(){
+  componentDidCatch() {
     console.log("error");
     this.setState({
-      error: true
-    })
+      error: true,
+    });
   }
 
   toggleRandomChar = () => {
     this.setState((state) => {
       return {
-        showRandomChar: !state.showRandomChar
-      } 
+        showRandomChar: !state.showRandomChar,
+      };
     });
   };
 
-
   render() {
     const char = this.state.showRandomChar ? <RandomChar /> : null;
-    if(this.state.error){
-      return <ErrorMessage/>
-    }  
+    if (this.state.error) {
+      return <ErrorMessage />;
+    }
+
     return (
-      <>
-        <Container>
-          <Header />
-        </Container>
-        <Container>
-          <Row>
-            <Col lg={{ size: 5, offset: 0 }}>
-            <button
-                type="button"
-                class="btn btn-info"
-                onClick={this.toggleRandomChar}>
-                Show/hide component
-              </button>
-              {char}
-            </Col>
-          </Row>
-          <CharactedPage/><br/>
-          <BooksPage/><br/>
-          <HousePage/><br/>
-        </Container>
-      </>
+      <Router>
+       <div className="App">
+          <Container>
+            <Header />
+          </Container>
+          <Container>
+            <Row>
+              <Col lg={{ size: 5, offset: 0 }}>
+                <button
+                  type="button"
+                  class="btn btn-info"
+                  onClick={this.toggleRandomChar}
+                >
+                  Show/hide component
+                </button>
+                {char}
+              </Col>
+           
+            </Row>
+            <Routes>
+                <Route path = '/characters' element = {<CharactedPage/>}/>
+                <Route path = '/books' element = {<BooksPage/>}/>
+                <Route path = '/houses' element = {<HousePage/>}/>
+              </Routes>
+               
+          </Container>
+        </div>
+      </Router>
+   
+       
+      
     );
-  }
+  } 
 }
